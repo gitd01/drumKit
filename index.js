376d3096c82f9a5738130009ab4e805362f562f1
gitd01 <110153941+gitd01@users.noreply.github.com>
2022-08-15 23:01:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelectorAll("button");
+// document.querySelectorAll("button")[0].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[1].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[2].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[3].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[4].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[5].addEventListener("click",handleclick);
+// document.querySelectorAll("button")[6].addEventListener("click",handleclick);
+
+var no_of_buttons=document.querySelectorAll(".drum").length;
+for(var i=0;i<no_of_buttons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+}
+
+function handleclick(){
+    var sound_no=Math.floor(Math.random()*6)+1;
+     var audio=new Audio("sound/"+sound_no+".mp3");
+     audio.play();
+}
