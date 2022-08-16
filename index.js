2136c4323616e1be4925af803d90735df803b841
Divyanshi <guptadivyanshi047@gmail.com>
2022-08-16 15:28:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,37 +9,37 @@ for(var i=0;i<no_of_buttons;i++){
         case'a':
             var audio=new Audio("sounds/0.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'b':
             var audio=new Audio("sounds/1.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'c':
             var audio=new Audio("sounds/2.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'd':
             var audio=new Audio("sounds/3.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'e':
             var audio=new Audio("sounds/4.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'f':
             var audio=new Audio("sounds/5.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         case'g':
             var audio=new Audio("sounds/6.mp3");
             audio.play();
-            console.log(this.style.color="darkblue");
+            // console.log(this.style.color="darkblue");
             break;
         default:
             console.log(buttonInnerHTML);
